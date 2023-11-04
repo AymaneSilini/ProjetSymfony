5e023d7baff4c7873cc539e21202942b24bbcff3
AymaneSilini <you@example.com>
2023-11-04 19:11:41 +0100

    CategoriesFixtures slug

--- EcommerceSf/src/DataFixtures/CategoriesFixtures.php
+++ EcommerceSf/src/DataFixtures/CategoriesFixtures.php
@@ -5,15 +5,28 @@ namespace App\DataFixtures;
 use App\Entity\Categories;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class CategoriesFixtures extends Fixture
+
 {
+    public function __construct(private SluggerInterface $slugger)
+    {
+    }
+
     public function load(ObjectManager $manager): void
     {
+        $parent = new Categories();
+        $parent->setName('Informatique');
+        $parent->setSlug($this->slugger->slug($parent->getName()));
+        $manager->persist($parent);
+
         $category = new Categories();
-        $category->setName('Informatique');
-        $category->setSlug('informatique');
+        $category->setName('Ordinateur portable');
+        $category->setSlug($this->slugger->slug($category->getName()));
+        $category->setParent($parent);
         $manager->persist($category);
+
         $manager->flush();
     }
 }
