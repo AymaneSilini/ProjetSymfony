326a60ef073e8fa442f94bf7802f94ba9c11310d
AymaneSilini <you@example.com>
2023-12-21 20:12:42 +0100

    isVerified flash

--- EcommerceSf/src/Entity/Users.php
+++ EcommerceSf/src/Entity/Users.php
@@ -192,6 +192,24 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
+
+    public function getIsVerified(): ?bool
+    {
+        return $this->is_verified;
+    }
+
+    public function setIsVerified(bool $is_verified): static
+    {
+        $this->is_verified = $is_verified;
+
+        return $this;
+    }
+
+
+
+
+
+
     /**
      * @return Collection<int, Orders>
      */
