9376877ed4a4db676c7dc32a798650fbc857daab
AymaneSilini <you@example.com>
2023-12-26 17:16:09 +0100

    JWTService

---
+++ EcommerceSf/src/Service/JWTService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Service;
+
+use DateTimeImmutable;
+
+class JWTService
+{
+    /**
+     * @param array $header
+     *  @param array $payload
+     *  @param string $secret
+     *  @param int $validity
+     * @return string
+     */
+    public function generate(array $header, array $payload, string $secret, int $validity = 10800): string
+    {
+        //if the token is not valid
+        if ($validity <= 0) {
+            return "";
+        }
+        //get get the actual datetime
+        $now = new DateTimeImmutable();
+        //we add the validity to the actual datetime, to know when it expires
+        $exp = $now->getTimestamp() + $validity;
+        //issued at and expired at
+        $payload['iat'] = $now->getTimestamp();
+        $payload['exp'] = $exp;
+        //we encode it in base64
+        $base64Header = base64_encode(json_encode($header));
+        $base64Payload = base64_encode(json_encode($payload));
+        //we clear the encoded value(+,/,=)
+        $base64Header = str_replace(['+', '/', '='], ['-', '_', ''], $base64Header);
+        $base64Payload = str_replace(['+', '/', '='], ['-', '_', ''], $base64Payload);
+        // we have to generate the signature, with a secret
+        $secret = base64_encode($secret);
+        $signature = hash_hmac('sha256', $base64Header . '.' . $base64Payload, $secret, true);
+        $base64Signature = base64_encode($signature);
+        $base64Signature = str_replace(['+', '/', '='], ['-', '_', ''], $base64Signature);
+        //we create the token
+        $jwt = $base64Header . '.' . $base64Payload . '.' . $base64Signature;
+        return $jwt;
+    }
+}
