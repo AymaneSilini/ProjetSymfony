e2ae049326eb9f330d3cda5cf5ed2b25229ba184
AymaneSilini <you@example.com>
2024-01-11 21:00:03 +0100

    pagination products

--- EcommerceSf/src/Controller/CategoriesController.php
+++ EcommerceSf/src/Controller/CategoriesController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Categories;
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,9 +13,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoriesController extends AbstractController
 {
     #[Route('/{slug}', name: 'list')]
-    public function list(Categories $categories): Response
+    public function list(Categories $categories, ProductsRepository $productsRepository, Request $request): Response
     {
-        $products = $categories->getProducts();
+        $page = $request->query->getInt('page', 1);
+        $products =  $productsRepository->findProductsPagination($page, $categories->getSlug(), 3);
         return $this->render('categories/list.html.twig', compact('categories', 'products'));
     }
 }
--- EcommerceSf/src/Repository/ProductsRepository.php
+++ EcommerceSf/src/Repository/ProductsRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Products;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -21,28 +22,48 @@ class ProductsRepository extends ServiceEntityRepository
         parent::__construct($registry, Products::class);
     }
 
-//    /**
-//     * @return Products[] Returns an array of Products objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findProductsPagination(int $page, string $slug, int $limit = 6): array
+    {
+        $limit = abs($limit);
+        $result = [];
+        $query =  $this->createQueryBuilder('p')->select('c', 'p')->join('p.categories', 'c')->where("c.slug = '$slug' ")->setMaxResults($limit)->setFirstResult(($page * $limit) - $limit);
+        $paginator = new Paginator($query);
+        $data = $paginator->getQuery()->getResult();
+        if (empty($data)) {
+            return $result;
+        }
+        //calculate the number of pages
+        $pages = ceil($paginator->count() / $limit);
+        //we fill the array with data, pages equal the total number of pages, page is the actual page, and limit
+        $result['data'] = $data;
+        $result['pages'] = $pages;
+        $result['page'] = $page;
+        $result['limit'] = $limit;
+        return $result;
+    }
 
-//    public function findOneBySomeField($value): ?Products
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Products[] Returns an array of Products objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('p.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Products
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
